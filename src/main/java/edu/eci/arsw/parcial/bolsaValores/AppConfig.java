60e87533c8f35b2f0f7ff865b8a74ead313fa94e
Juanfe8ag <juan.ochoa-g@mail.escuelaing.edu.co>
2025-09-20 09:43:21 -0500

    Update AppConfig.java

--- src/main/java/edu/eci/arsw/parcial/bolsaValores/AppConfig.java
+++ src/main/java/edu/eci/arsw/parcial/bolsaValores/AppConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "edu.eci.arsw.parcial.bolsaValores")
 public class AppConfig {
 
 }
